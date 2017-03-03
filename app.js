c0c3155c204e599c57fa1ce6d9330b2af70c14ae
Andrew Cox <drew.r.cox@gmail.com>
2017-03-02 23:33:34 -0500

    Updated form and aside html; add layout design for js-post, form, and aside; added more jquery to post

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
-getDataFromApi('new york giants', function (data) {
-  data.response.docs.forEach(function (item) {
-    var htmlItem = $('article.hidden').clone();
-    htmlItem.find('.card-title').text(item.headline.main).click(function (event) {
-      openArticle(item);
-    });
-    htmlItem.find('.js-snippet').text(item.snippet);
-    var imageFound = item.multimedia.find(function (image) {
-      return image.subtype === 'thumbnail';
-    });
-    if (imageFound) {
-      htmlItem.find('img').attr('src', 'http://www.nytimes.com/' + imageFound.url);
-    } else {
-      htmlItem.find('img').attr('src', 'timeslogo.png');
-    }
-    htmlItem.removeClass('hidden');
-    $('section #divlist').append(htmlItem);
-  });
-});
+var state = {};
 
-function getDataFromApi (term, callback) {
+function getDataFromApi(term, callback) {
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-  url += '?' + $.param({
-    'api-key': "f8d4f007b2584ff48d7b7177f0f4008c",
-    q: term
-  });
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
+  url += '?' + $.param({'api-key': "f8d4f007b2584ff48d7b7177f0f4008c", q: term});
+  $.ajax({url: url, method: 'GET'}).done(function(result) {
     console.log(result);
     callback(result);
   }).fail(function(err) {
@@ -35,24 +11,75 @@ function getDataFromApi (term, callback) {
   });
 }
 
-function buildPost () {
-  //show url
-  //headline
-  //section
-  //date
-  //image
-  //text
-}
-
-function openArticle (article) {
-  $('.js-post h3').text(article.section_name);
-  $('section').hide();
-  $('.js-post').show();
+function openArticle(article) {
+  $('.js-post h1').text(article.headline.main);
+  var imageFound = article.multimedia.find(function(image) {
+    return image.subtype === 'xlarge';
+  });
+  if (imageFound) {
+    $('.js-post img').attr('src', 'http://www.nytimes.com/' + imageFound.url);
+  }
+  $('.author span').text(article.byline.original);
+  $('.date span').text(article.pub_date);
+  $('.area span').text(article.section_name + ' Section');
+  $('.js-post p').text(article.lead_paragraph);
+  $('.js-post a').attr('href', article.web_url);
+  $('.article-list').hide();
+  $('.search h2').hide();
+  $('.search p').hide();
+  $('.search').css('margin-top', '20px');
+  $('.wrapper').show();
   $('aside').show();
 }
 
-function closeArticle () {
-  $('.js-post').hide();
+function closeArticle() {
+  $('.wrapper').hide();
   $('aside').hide();
-  $('section').show();
+  $('.article-list').show();
 }
+
+function submitQuery() {
+  state.query = $('#js-search').val();
+  localStorage.query = state.query;
+  getDataFromApi(state.query, function(data) {
+    data.response.docs.forEach(function(item) {
+      $('ul').append('<li>' + item.headline.main +'</li>');
+      var htmlItem = $('article.hidden').clone();
+      htmlItem.find('.card-title').text(item.headline.main);
+      $('.card').click(function(event) {
+        openArticle(item);
+      });
+      htmlItem.find('.js-snippet').text(item.snippet);
+      var imageFound = item.multimedia.find(function(image) {
+        return image.subtype === 'thumbnail';
+      });
+      if (imageFound) {
+        htmlItem.find('img').attr('src', 'http://www.nytimes.com/' + imageFound.url);
+      } else {
+        htmlItem.find('img').attr('src', 'timeslogo.png');
+      }
+      htmlItem.removeClass('hidden');
+      $('section #divlist').append(htmlItem);
+    });
+  });
+}
+
+$(function() {
+  if ('query' in localStorage) {
+    state.query = localStorage.query;
+    $('#js-search').val(state.query);
+    submitQuery();
+  }
+  Materialize.updateTextFields();
+  $('form').submit(function(event) {
+    event.preventDefault();
+    //$('.results').detach();
+    submitQuery();
+  })
+  $('#search-icon').click(function() {
+    $('form').submit();
+  });
+  $('.close').click(function() {
+    closeArticle();
+  });
+});
