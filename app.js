6b94f474288d9c1469c518da84d94328d6bef1d6
Andrew Cox <drew.r.cox@gmail.com>
2017-09-22 23:03:40 -0400

    Clean up some code and begin es6 conversion

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var state = {
+const state = {
   beginDate: 18510918,
   endDate: 20200101,
   page: 0
 };
 
 function getDataFromApi(term, beginDate, endDate, pageNum, sortOrder, callback) {
-  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  const url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
   url += '?' + $.param({
     'api-key': "f8d4f007b2584ff48d7b7177f0f4008c",
     'q': term,
@@ -16,16 +16,19 @@ function getDataFromApi(term, beginDate, endDate, pageNum, sortOrder, callback) 
   if (sortOrder) {
     url += '&sort=' + sortOrder;
   }
-  $.ajax({url: url, method: 'GET'}).done(function(result) {
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function (result) {
     callback(result);
-  }).fail(function(err) {
+  }).fail((err) => {
     throw err;
   });
 }
 
 function openArticle(article) {
   $('.js-post h4').text(article.headline.main);
-  var imageFound = article.multimedia.find(function(image) {
+  const imageFound = article.multimedia.find((image) => {
     return image.subtype === 'xlarge';
   });
   if (imageFound) {
@@ -38,12 +41,12 @@ function openArticle(article) {
   }
 
   //Have to format the date so it's readable
-  var dateFormat = article.pub_date.slice(0, 10).split("-");
-  Array.prototype.move = function (from, to) {
+  const dateFormat = article.pub_date.slice(0, 10).split("-");
+  Array.prototype.move = (from, to) => {
     this.splice(to, 0, this.splice(from, 1)[0]);
     return this;
   };
-  var newDateFormat = dateFormat.move(0, 2).join('/');
+  const newDateFormat = dateFormat.move(0, 2).join('/');
   $('.date span').text(newDateFormat);
 
   if (article.section_name) {
@@ -65,15 +68,15 @@ function closeArticle() {
   $('.article-list').show();
 }
 
-function closure (item) {
-  return function(event) {
+function closure(item) {
+  return (event) => {
     openArticle(item);
   };
 }
 
 function previousPage() {
-  $('#previous').click(function() {
-    state.page --;
+  $('#previous').click(function () {
+    state.page--;
     $('#next').prop('disabled', false);
     if (state.page === 0) {
       $('#previous').prop('disabled', true);
@@ -83,8 +86,8 @@ function previousPage() {
 }
 
 function nextPage() {
-  $('#next').click(function() {
-    state.page ++;
+  $('#next').click(function () {
+    state.page++;
     $('#previous').prop('disabled', false);
     if (state.page === 10) {
       $('#next').prop('disabled', true);
@@ -94,7 +97,7 @@ function nextPage() {
 }
 
 function clearSearch() {
-  $('.js-clear').click(function() {
+  $('.js-clear').click(function () {
     $("#js-search, textarea").val("");
     submitQuery();
     location.reload();
@@ -106,28 +109,28 @@ function submitQuery() {
   $('#divlist').empty();
   state.query = $('#js-search').val();
   if ($('#js-beginDate').val()) {
-    var beginDate = $('#js-beginDate').val();
-    var dateArray = beginDate.split('-');
+    let beginDate = $('#js-beginDate').val();
+    let dateArray = beginDate.split('-');
     state.beginDate = dateArray[0] + dateArray[1] + dateArray[2];
     localStorage.beginDate = state.beginDate;
   }
   if ($('#js-endDate').val()) {
-    var endDate = $('#js-endDate').val();
-    var dateArray = endDate.split('-');
+    let endDate = $('#js-endDate').val();
+    let dateArray = endDate.split('-');
     state.endDate = dateArray[0] + dateArray[1] + dateArray[2];
     localStorage.endDate = state.endDate;
   }
   localStorage.query = state.query;
-  getDataFromApi(state.query, state.beginDate, state.endDate, state.page, state.sort, function(data) {
-    data.response.docs.forEach(function(item) {
-      var htmlItem = $('article.hidden').clone();
+  getDataFromApi(state.query, state.beginDate, state.endDate, state.page, state.sort, function (data) {
+    data.response.docs.forEach(function (item) {
+      const htmlItem = $('article.hidden').clone();
       if (item.headline.main.length > 115) {
-        item.headline.main = item.headline.main.substring(0,130) + '...';
+        item.headline.main = item.headline.main.substring(0, 130) + '...';
       }
       htmlItem.find('.card-title').text(item.headline.main);
       htmlItem.find('.card').click(closure(item));
       htmlItem.find('.js-snippet').text(item.snippet);
-      var imageFound = item.multimedia.find(function(image) {
+      const imageFound = item.multimedia.find(function (image) {
         return image.subtype === 'thumbnail';
       });
       if (imageFound) {
@@ -144,14 +147,12 @@ function submitQuery() {
   });
 }
 
-$(function() {
-  //$('#js-beginDate').mask('00/00/0000');
-  //$('#js-endDate').mask('00/00/0000');
+$(function () {
   previousPage();
   nextPage();
   clearSearch();
   $('select').material_select();
-  $('#selector').change(function(event) {
+  $('#selector').change((event) => {
     state.sort = event.target.value;
     submitQuery();
   })
@@ -161,15 +162,15 @@ $(function() {
     submitQuery();
   }
   Materialize.updateTextFields();
-  $('form').submit(function(event) {
+  $('form').submit((event) => {
     event.preventDefault();
     $('section #divlist').empty();
     submitQuery();
-  })
-  $('#search-icon').click(function() {
+  });
+  $('#search-icon').click(() => {
     $('form').submit();
   });
-  $('.close').click(function() {
+  $('.close').click(() => {
     closeArticle();
   });
 });
